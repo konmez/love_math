2cb4a9256fa2738d6a49e48295dbcb6c22fa0501
konmez <kmezin@yahoo.com>
2024-11-14 21:53:50 +0000

    add js structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
 console.log('hello from script');
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
