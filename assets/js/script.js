23fba9c549cf61c015a6e8738504ddbf040cc461
konmez <kmezin@yahoo.com>
2024-11-15 10:25:27 +0000

    add additionFdisplayAdditionQuestion content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,47 @@
 console.log('hello from script');
-function runGame() {
+
+document.addEventListener('DOMContentLoaded',function()
+  {
+    let buttons = document.getElementsByTagName('button');    
+    for (let button of buttons)
+        {
+            button.addEventListener('click', function()
+            {
+                if (this.getAttribute("data-type") === "submit")
+                    {alert('submit clicked');
+                    }
+                else {
+                    let gameType = this.getAttribute("data-type");
+                    alert(` You clicked ${gameType}`)       ;
+                    runGame(gameType ) 
+                }    
+            })
+        }
+
+    runGame('addition')    ;
+  })
+
+/**
+* The main game 'looop', called when the script is first loaded
+* and after the user's answer has been processed
+*/
+
+function runGame(gameType) {
+    let num1 =Math.floor(Math.random()*25)+1;
+    let num2 =Math.floor(Math.random()*25)+1;
+    console.log('rg',num1, num2);
+    if (gameType === "addition"){
+        displayAdditionQuestion(num1,num2);
+    } else {
+        { alert(`Unknown game type:  ${gameType}`);
+            throw `Unknown game type: ${gameType}. ABORTED!`;
+        }
+    }
+    
 
 }
+
+
 
 function checkAnswer() {
 
@@ -19,8 +59,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1;
+  document.getElementById('operand2').textContent = operand2;
+  document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
