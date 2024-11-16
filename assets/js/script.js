93cd264482106ca0855f666dcbeaecf51d28cb28
konmez <kmezin@yahoo.com>
2024-11-16 12:00:32 +0000

    kind of done

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,6 +154,5 @@ function displayMultiplyQuestion(operand1, operand2) {
 function displayDivisionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = parseInt(operand1)*parseInt(operand2);
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "÷";
-    
+    document.getElementById('operator').textContent = "÷";    
 }
